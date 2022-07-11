25ba8dba084aab0ff65189907f5aa0de65deec42
Jack Devey <jack@bandev.uk>
2022-07-11 20:21:07 +0100

    Commit stuff i forgot to commit

---
+++ client/misc/style/Style.ts
@@ -0,0 +1,20 @@
+export function BackgroundStyle(theme) { 
+    return {
+        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[0],
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: 'column' as 'column',
+        alignItems: "center", 
+        justifyContent: "center",
+    }
+}
+
+export function CompactLineStyle() {
+    return {
+        marginTop: "0px",
+        marginBottom: "0xp",
+        paddingTop: "0px",
+        paddingBottom: "0xp"
+    }
+
+}

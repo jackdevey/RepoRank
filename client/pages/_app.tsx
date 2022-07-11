b859581879a36f4663650c7556c53019e67779a7
Jack Devey <jack@bandev.uk>
2022-07-11 21:18:08 +0100

    Fix favicon

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -20,7 +20,7 @@ export default function App(props: AppProps) {
       <Head>
         <title>RepoRank</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        <link rel="apple-touch-icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔥</text></svg>" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔥</text></svg>" />
       </Head>
 
       <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>

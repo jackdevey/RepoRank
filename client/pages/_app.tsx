e98bebf2b7eec4790922db2ae414860a42d74175
Jack Devey <jack@bandev.uk>
2022-07-20 05:10:17 +0100

    Remove font as it caused error

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -22,9 +22,6 @@ export default function App(props: AppProps) {
         <title>RepoRank</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔥</text></svg>" />
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com"/>
-        <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@700&display=swap" rel="stylesheet"/>
       </Head>
 
       <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>

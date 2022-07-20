96a031826df8f97c1c4351aebbcb56fec239cbda
Jack Devey <jack@bandev.uk>
2022-07-20 04:56:42 +0100

    Fix some bugs with repos

--- client/components/userrank/ReposBlock/index.tsx
+++ client/components/userrank/ReposBlock/index.tsx
@@ -1,5 +1,4 @@
-import { createStyles, Grid, Paper, Text, ThemeIcon, Container, Badge, Title, Anchor } from "@mantine/core";
-import { ColorSwatch } from "tabler-icons-react";
+import { createStyles, Grid, Paper, Text, Container, Badge, Title, Anchor } from "@mantine/core";
 
 function RepoCard({ repo } ) {
     const useStyles = createStyles((theme) => ({
@@ -26,8 +25,9 @@ function RepoCard({ repo } ) {
 
     const { classes } = useStyles();
     return (
-        <Paper withBorder radius="md" className={classes.card}>
-            {repo.primaryLanguage && (<Badge radius="md" variant="gradient" gradient={{ from: repo.primaryLanguage.color, to: repo.primaryLanguage.color, deg: 69 }}>{repo.primaryLanguage.name}</Badge>)}
+        <Paper withBorder radius="md" className={classes.card} style={{ minHeight: 210 }}>
+            {repo.primaryLanguage && (<Badge radius="md" variant="gradient" gradient={{ from: repo.primaryLanguage.color, to: repo.primaryLanguage.color, deg: 69 }}>{repo.primaryLanguage.name}</Badge>)
+                || <Badge radius="md">No Language</Badge> }
             <Title order={3} mt="sm"><Anchor href={repo.url} inherit target="_blank" className={classes.title}>{repo.name}</Anchor></Title>
             <Text size="sm" mt="sm">
                 <Badge color="gray" leftSection="⭐">{repo.stargazerCount}</Badge>&nbsp;
@@ -45,14 +45,22 @@ export function ReposBlock({ repos }) {
             position: 'relative',
             paddingTop: theme.spacing.xl * 4,
             paddingBottom: theme.spacing.xl * 4,
-          },
+        },
+        title: {
+            marginBottom: theme.spacing.sm,
+        
+            [theme.fn.smallerThan('sm')]: {
+              fontSize: 28,
+            },
+        },
     }));
     const { classes } = useStyles();
     return (
         <>
             <Container className={classes.wrapper}>
-                <Title mb="xl">Your top 3 repositories</Title>
-                <Grid>
+                <Title className={classes.title}>Your top 3 repositories</Title>
+                <Text>Ordered by number of stars</Text>
+                <Grid mt={60} gutter={30}>
                 {repos.map((repo, index) => 
                     <Grid.Col sm={12} md={6} lg={4} key={index}>
                        <RepoCard repo={repo} />

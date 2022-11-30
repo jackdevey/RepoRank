d5411b40c467c82b1be5af5fc15d3997f29bc07d
Jack Devey <jack@jw3.uk>
2022-11-30 10:18:06 +0000

    make data fluid

--- apps/web/pages/[owner]/[repo].tsx
+++ apps/web/pages/[owner]/[repo].tsx
@@ -11,8 +11,105 @@ export default function Repo() {
     const router = useRouter();
     const { owner, repo } = router.query;
 
-  const theme = useMantineTheme();
-  const SECONDARY_COL_HEIGHT = PRIMARY_COL_HEIGHT / 2 - theme.spacing.md / 2;
+    const theme = useMantineTheme();
+    const data = {
+        about: {
+            owner,
+            repo,
+            type: "Standard",
+            language: {
+                name: "Kotlin",
+                colour: "orange"
+            }
+        },
+        metrics: [
+            {
+                title: "Overview",
+                metrics: [
+                    {
+                        name: "Stars",
+                        value: 678000,
+                        points: 678,
+                        maxPoints: 1000,
+                        system: "k"
+                    },
+                    {
+                        name: "Forks",
+                        value: 34600,
+                        points: 346,
+                        maxPoints: 1000,
+                        system: "h"
+                    },
+                    {
+                        name: "Watchers",
+                        value: 2820,
+                        points: 282,
+                        maxPoints: 1000,
+                        system: "da"
+                    }
+                ]
+            },
+            {
+                title: "Activity",
+                metrics: [
+                    {
+                        name: "Commits this year",
+                        value: 789,
+                        points: 789,
+                        maxPoints: 1000,
+                        system: "-"
+                    },
+                    {
+                        name: "Last commit",
+                        value: "2 days ago",
+                        points: 600,
+                        maxPoints: 1000,
+                        system: "dt"
+                    },
+                    {
+                        name: "Open issues",
+                        value: 12,
+                        points: 120,
+                        maxPoints: 1000,
+                        system: "da"
+                    }
+                ]
+            },
+            {
+                title: "Community Standards",
+                metrics: [
+                    {
+                        name: "Readme",
+                        value: "Yes",
+                        points: 150,
+                        maxPoints: 150,
+                        system: "-"
+                    },
+                    {
+                        name: "Code of conduct",
+                        value: "Yes",
+                        points: 150,
+                        maxPoints: 150,
+                        system: "-"
+                    },
+                    {
+                        name: "Contributing",
+                        value: "Yes",
+                        points: 150,
+                        maxPoints: 150,
+                        system: "-"
+                    },
+                    {
+                        name: "Security Policy",
+                        value: "No",
+                        points: 0,
+                        maxPoints: 150,
+                        system: "-"
+                    }
+                ]
+            }
+        ]
+    };
 
     return (
         <>
@@ -32,7 +129,7 @@ export default function Repo() {
                         <Card withBorder>
                             <Title order={4}>Owner</Title>
                             <Flex style={{alignItems: "baseline"}}>
-                                <Title>russellbanks</Title>
+                                <Title>{data.about.owner}</Title>
                                 <ActionIcon variant="subtle" color="teal">a</ActionIcon>
                             </Flex>
                         </Card>
@@ -40,13 +137,13 @@ export default function Repo() {
                     <Grid.Col span={6}>
                         <Card withBorder>
                             <Title order={4}>Type</Title>
-                            <Title>Standard</Title>
+                            <Title>{data.about.type}</Title>
                         </Card>
                     </Grid.Col>
                     <Grid.Col span={6}>
                         <Card withBorder>
                             <Title order={4}>Primary language</Title>
-                            <Title>Kotlin</Title>
+                            <Title>{data.about.language.name}</Title>
                         </Card>
                     </Grid.Col>
                     </Grid>
@@ -54,94 +151,10 @@ export default function Repo() {
 
                 <Title mt="lg">Metrics</Title>
                 <Divider mt="sm" mb="md" />
-                <MetricGroupSection group={{
-                    title: "Overview",
-                    metrics: [
-                        {
-                            name: "Stars",
-                            value: 678000,
-                            points: 678,
-                            maxPoints: 1000,
-                            system: "k"
-                        },
-                        {
-                            name: "Forks",
-                            value: 34600,
-                            points: 346,
-                            maxPoints: 1000,
-                            system: "h"
-                        },
-                        {
-                            name: "Watchers",
-                            value: 2820,
-                            points: 282,
-                            maxPoints: 1000,
-                            system: "da"
-                        }
-                    ]
-                }} />
-                <Space mt="xl" mb="md" />
-                <MetricGroupSection group={{
-                    title: "Activity",
-                    metrics: [
-                        {
-                            name: "Commits this year",
-                            value: 789,
-                            points: 789,
-                            maxPoints: 1000,
-                            system: "-"
-                        },
-                        {
-                            name: "Last commit",
-                            value: "2 days ago",
-                            points: 600,
-                            maxPoints: 1000,
-                            system: "dt"
-                        },
-                        {
-                            name: "Open issues",
-                            value: 12,
-                            points: 120,
-                            maxPoints: 1000,
-                            system: "da"
-                        }
-                    ]
-                }} />
-                <Space mt="xl" mb="md" />
-                <MetricGroupSection group={{
-                    title: "Community Standards",
-                    metrics: [
-                        {
-                            name: "Readme",
-                            value: "Yes",
-                            points: 150,
-                            maxPoints: 150,
-                            system: "-"
-                        },
-                        {
-                            name: "Code of conduct",
-                            value: "Yes",
-                            points: 150,
-                            maxPoints: 150,
-                            system: "-"
-                        },
-                        {
-                            name: "Contributing",
-                            value: "Yes",
-                            points: 150,
-                            maxPoints: 150,
-                            system: "-"
-                        },
-                        {
-                            name: "Secuirty Policy",
-                            value: "No",
-                            points: 0,
-                            maxPoints: 150,
-                            system: "-"
-                        }
-                    ]
-                }} />
-                <Space mt="xl" mb="md" />
+                {data.metrics.map((group) => <>
+                    <MetricGroupSection group={group}/>
+                    <Space mt="xl" mb="md" />
+                </>)}
              
             </Container>
         </>
--- packages/algs/index.ts
+++ packages/algs/index.ts
@@ -1,5 +1,102 @@
 import { graphql } from "@octokit/graphql";
 
 export function repo(owner: string, repo: string) {
-    return "a"
+    return {
+        about: {
+            owner,
+            repo,
+            type: "Standard",
+            language: {
+                name: "Kotlin",
+                colour: "orange"
+            }
+        },
+        metrics: [
+            {
+                title: "Overview",
+                metrics: [
+                    {
+                        name: "Stars",
+                        value: 678000,
+                        points: 678,
+                        maxPoints: 1000,
+                        system: "k"
+                    },
+                    {
+                        name: "Forks",
+                        value: 34600,
+                        points: 346,
+                        maxPoints: 1000,
+                        system: "h"
+                    },
+                    {
+                        name: "Watchers",
+                        value: 2820,
+                        points: 282,
+                        maxPoints: 1000,
+                        system: "da"
+                    }
+                ]
+            },
+            {
+                title: "Activity",
+                metrics: [
+                    {
+                        name: "Commits this year",
+                        value: 789,
+                        points: 789,
+                        maxPoints: 1000,
+                        system: "-"
+                    },
+                    {
+                        name: "Last commit",
+                        value: "2 days ago",
+                        points: 600,
+                        maxPoints: 1000,
+                        system: "dt"
+                    },
+                    {
+                        name: "Open issues",
+                        value: 12,
+                        points: 120,
+                        maxPoints: 1000,
+                        system: "da"
+                    }
+                ]
+            },
+            {
+                title: "Community Standards",
+                metrics: [
+                    {
+                        name: "Readme",
+                        value: "Yes",
+                        points: 150,
+                        maxPoints: 150,
+                        system: "-"
+                    },
+                    {
+                        name: "Code of conduct",
+                        value: "Yes",
+                        points: 150,
+                        maxPoints: 150,
+                        system: "-"
+                    },
+                    {
+                        name: "Contributing",
+                        value: "Yes",
+                        points: 150,
+                        maxPoints: 150,
+                        system: "-"
+                    },
+                    {
+                        name: "Security Policy",
+                        value: "No",
+                        points: 0,
+                        maxPoints: 150,
+                        system: "-"
+                    }
+                ]
+            }
+        ]
+    }
 }

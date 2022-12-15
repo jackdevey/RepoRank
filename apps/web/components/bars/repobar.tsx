9b6bfc52b8ec7757e2ea318aa8d4f9ac8852e933
Jack Devey <jack@jw3.uk>
2022-12-15 21:28:33 +0000

    Cleanups

--- apps/web/components/bars/repobar.tsx
+++ apps/web/components/bars/repobar.tsx
@@ -1,8 +1,11 @@
 import { Box, Divider, Flex, ThemeIcon, Title, Text, Container, ActionIcon } from "@mantine/core";
-import { Refresh } from "tabler-icons-react";
+import { useRouter } from "next/router";
+import { Refresh, Router } from "tabler-icons-react";
 import TierIcon from "../tiers/tierIcon";
 
 export default function Repobar({ owner, repo, tier, points}: RepobarProps) {
+    // Use next router
+    const router = useRouter()
     return <>
         <div style={{position: "sticky", top: 0, backgroundColor: "#0A2232", zIndex: 1000}}>
             <Box px="md">
@@ -10,7 +13,7 @@ export default function Repobar({ owner, repo, tier, points}: RepobarProps) {
                     <Flex style={{ justifyContent: "space-between", alignItems: "center" }} pt="md" pb="sm">
                         <Flex>
                             <TierIcon tier={tier}/>
-                            <Title ml="sm" mr="xs" order={3}>{owner}/{repo}</Title>
+                            <Title ml="sm" order={3}>{owner}/{repo}</Title>
                             <ActionIcon>
                                 <Refresh size={18}/>
                             </ActionIcon>

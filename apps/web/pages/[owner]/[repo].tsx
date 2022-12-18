8324c5ab0b8f9062234ab93eadb574ad70d19cca
Jack Devey <jack@jw3.uk>
2022-12-18 23:31:26 +0000

    More cool fixes

--- apps/web/pages/[owner]/[repo].tsx
+++ apps/web/pages/[owner]/[repo].tsx
@@ -10,116 +10,51 @@ import { useEffect, useState } from "react";
 
 const PRIMARY_COL_HEIGHT = 300;
 
+type RepoMetric = {
+    name: string,
+    value: number,
+    points: number,
+    maxPoints: number,
+    system: string
+}
+
+type RepoMetricGroup = {
+    title: string,
+    metrics: RepoMetric[]
+}
+
+type Repo = {
+    about: {
+        owner: string,
+        repo: string,
+        type: string,
+        language: string
+    },
+    metrics: RepoMetricGroup[]
+}
+
 export default function Repo() {
 
     const router = useRouter();
     const { owner, repo } = router.query;
 
-    const [data, setData] = useState({
-        about: {
-            owner,
-            repo,
-            type: "Standard",
-            language: {
-                name: "Kotlin",
-                colour: "orange"
-            }
-        },
-        metrics: [
-            {
-                title: "Overview",
-                metrics: [
-                    {
-                        name: "Stars",
-                        value: 678000,
-                        points: 678,
-                        maxPoints: 1000,
-                        system: "k"
-                    },
-                    {
-                        name: "Forks",
-                        value: 34600,
-                        points: 346,
-                        maxPoints: 1000,
-                        system: "h"
-                    },
-                    {
-                        name: "Watchers",
-                        value: 2820,
-                        points: 282,
-                        maxPoints: 1000,
-                        system: "da"
-                    }
-                ]
-            },
-            {
-                title: "Activity",
-                metrics: [
-                    {
-                        name: "Commits this year",
-                        value: 789,
-                        points: 789,
-                        maxPoints: 1000,
-                        system: "-"
-                    },
-                    {
-                        name: "Last commit",
-                        value: "2 days ago",
-                        points: 600,
-                        maxPoints: 1000,
-                        system: "dt"
-                    },
-                    {
-                        name: "Open issues",
-                        value: 12,
-                        points: 120,
-                        maxPoints: 1000,
-                        system: "da"
-                    }
-                ]
-            },
-            {
-                title: "Community Standards",
-                metrics: [
-                    {
-                        name: "Readme",
-                        value: "Yes",
-                        points: 150,
-                        maxPoints: 150,
-                        system: "-"
-                    },
-                    {
-                        name: "Code of conduct",
-                        value: "Yes",
-                        points: 150,
-                        maxPoints: 150,
-                        system: "-"
-                    },
-                    {
-                        name: "Contributing",
-                        value: "Yes",
-                        points: 150,
-                        maxPoints: 150,
-                        system: "-"
-                    },
-                    {
-                        name: "Security Policy",
-                        value: "No",
-                        points: 0,
-                        maxPoints: 150,
-                        system: "-"
-                    }
-                ]
-            }
-        ]
-    });
+    const [data, setData] = useState<Repo>();
 
     useEffect(() => {
-        getRepo(owner as string, repo as string).then(data => setData(data))
-      }, []) 
+        if(owner && repo) {
+            console.log(owner); console.log(repo);
+            getRepo(owner as string, repo as string).then(data => setData(data))
+        }
+    }, [owner, repo]) 
+
+    
 
 
     const theme = useMantineTheme();
+
+    if (!data) {
+        return <a>Loading</a>
+    }
 
     return (
         <AppShell
@@ -162,7 +97,7 @@ export default function Repo() {
                             <Grid.Col span={6}>
                                 <Card withBorder>
                                     <Title order={4}>Primary language</Title>
-                                    <Title>{data.about.language.name}</Title>
+                                    <Title>{data.about.language}</Title>
                                 </Card>
                             </Grid.Col>
                         </Grid>
